b06f5450a863b3dcf9cfc934d3ba9a35cc7a71ca
Os-Carlos <78707106+Os-Carlos@users.noreply.github.com>
2023-08-29 14:21:06 -0600

    Update db.config.js

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -2,7 +2,7 @@ module.exports = {
     HOST: "dpg-cjn50udhe99c73b4r6fg-a",
     USER: "root",
     PASSWORD: "dOeByql5kuGNjpeF7FdzK4fohvnrHhtE",
-    DB: "proyecto",
+    DB: "proyecto_sp4r",
     dialect: "postgres",
     pool: {
         max: 5,
